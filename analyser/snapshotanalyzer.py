363b6fd17b6ca46aa7a12ad7e0c71ff0fe087f9b
goffer123 <goffersoft@gmail.com>
2020-03-17 16:12:54 -0400

    more updates

--- analyser/snapshotanalyzer.py
+++ analyser/snapshotanalyzer.py
@@ -1,4 +1,5 @@
 import boto3
+import click 
 
 def get_aws_session(profile_name):
     return boto3.Session(profile_name=profile_name)
@@ -12,11 +13,28 @@ def get_ec2_instances(ec2_resource_object):
         instances.append(i)
     return instances 
 
-def main_loop():
-    session = get_aws_session('goffer-snapshotanalyzer')
-    ec2_resource  = get_aws_resource(session, 'ec2')
-    ec2_instances = get_ec2_instances(ec2_resource)
-    for i in ec2_instances: print(i)
+def print_ec2_instances(ec2_instances):
+    for i in ec2_instances:
+        print(','.join((
+                 i.id,
+                 i.instance_type,
+                 i.placement['AvailabilityZone'],
+                 i.state['Name'],
+                 i.public_dns_name))
+             )
+
+    return
+
+@click.command()
+def list_ec2_instances():
+    """List EC2 Instances"""
+    ec2_resource_object = init('goffer-snapshotanalyzer', 'ec2')
+    ec2_instances = get_ec2_instances(ec2_resource_object)
+    print_ec2_instances(ec2_instances)
+
+def init(profile_name, resource_name):
+    session = get_aws_session(profile_name)
+    return get_aws_resource(session, resource_name)
 
 if __name__ == "__main__":
-    main_loop()
+    list_ec2_instances()
