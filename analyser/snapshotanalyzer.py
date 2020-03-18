dd664dbbd56d0bfc0d8e6ec314a56a65cdcd1740
goffer123 <goffersoft@gmail.com>
2020-03-18 12:24:58 -0400

    refactor

--- analyser/snapshotanalyzer.py
+++ analyser/snapshotanalyzer.py
@@ -1,78 +1,96 @@
 import boto3
-import click 
+import click
 
-def get_aws_session(profile_name):
-    return boto3.Session(profile_name=profile_name)
+g_aws_session = None
+g_ec2_resource = None
 
-def get_aws_resource(session, resource_name):
-    return session.resource(resource_name)
+def get_session(pname):
+    return boto3.Session(profile_name=pname)
 
-def get_ec2_instances(ec2_resource_object, project):
-    instances = []
+def get_resource(session, rname):
+    return session.resource(rname)
 
+def get_instances(resource, project):
     if (project == None):
-        instlist = ec2_resource_object.instances.all()
-    else:
-        filters=[{'Name':'tag:Project', 'Values':[project]}]
-        instlist = ec2_resource_object.instances.filter(Filters=filters)
+        return list(resource.instances.all())
+    
+    filter=[{'Name':'tag:Project', 'Values':[project]}]
 
-    for i in instlist:
-        instances.append(i)
-    return instances 
+    return list(resource.instances.filter(Filters=filter))
 
-def print_ec2_instances(ec2_instances):
-    for i in ec2_instances:
+def list_ec2_instances(project):
+    """ List EC2 instances """
+    for i in get_instances(g_ec2_resource, project):
         tags = {t['Key']:t['Value'] for t in i.tags or []}
-        print(','.join((
-                 i.id,
-                 i.instance_type,
-                 i.placement['AvailabilityZone'],
-                 i.state['Name'],
-                 i.public_dns_name,
-                 'Project' + ':' + tags.get('Project', '<no project>') ))
-             )
+        print(','.join((i.id,
+                        i.instance_type,
+                        i.public_dns_name,
+                        i.placement['AvailabilityZone'],
+                        i.state['Name'],
+                        tags.get('Project', '<no-project>'))))
     return
+
+def start_ec2_instances(project):
+    """ Start EC2 instances """
+    for i in get_instances(g_ec2_resource, project):
+        print('Starting...{0}'.format(i.id))
+        i.start() 
+    
+    return 
+
+def stop_ec2_instances(project):
+    """ Stop EC2 instances """
+    for i in get_instances(g_ec2_resource, project):
+        print('Stopping...{0}'.format(i.id))
+        i.stop() 
+    
+    return 
+
+def init(pname, rname):
+    """ initialze boto3 """
+
+    session = get_session(pname)
+    resource = get_resource(session, rname)
+
+    return (session, resource)
 
 @click.group()
 def instances():
-    """Commands for Instances"""
+    """ Commands related to instances """
 
-@instances.command('start')
-@click.option('--project', default=None,
-               help='only isntances for project(tag Project:<name>')
-def start_instances(project):
-    """ Start EC2 Instances """
-
-    ec2_resource_object = init('goffer-snapshotanalyzer', 'ec2')
-    ec2_instances = get_ec2_instances(ec2_resource_object, project)
-    for i in ec2_instances:
-        print('Starting... {0}'.format(i.id))
-        i.start()
-
-@instances.command('stop')
-@click.option('--project', default=None,
-              help='only instances for project(tag Project:<name>)')
-def stop_instances(project):
-    """ Stop EC2 Instances """
-    ec2_resource_object = init('goffer-snapshotanalyzer', 'ec2')
-    ec2_instances = get_ec2_instances(ec2_resource_object, project)
-    
-    for i in ec2_instances:
-        print("Stopping... {0}".format(i.id))
-        i.stop()
+    return
 
 @instances.command('list')
 @click.option('--project', default=None,
-        help='Only instances for project (tag Project:<name>)')
-def list_ec2_instances(project):
-    """List EC2 Instances"""
-    ec2_resource_object = init('goffer-snapshotanalyzer', 'ec2')
-    ec2_instances = get_ec2_instances(ec2_resource_object, project)
-    print_ec2_instances(ec2_instances)
+           help='print all ec2 isntances for project tag:Project:<name>')
+def list_instances(project):
+    """ List instances """
 
-def init(profile_name, resource_name):
-    session = get_aws_session(profile_name)
-    return get_aws_resource(session, resource_name)
+    list_ec2_instances(project)
+
+    return
+
+@instances.command('start')
+@click.option('--project', default=None,
+             help='start all ec2 isntances for project tag:Project:<name>')
+def start_instances(project):
+    """ Start instances """
+    
+    start_ec2_instances(project)
+
+    return
+
+@instances.command('stop')
+@click.option('--project', default=None,
+             help='stop all ec2 isntances for project tag:Project:<name>')
+def stop_instances(project):
+    """ Stop instances """
+
+    stop_ec2_instances(project)
+
+    return
 
 if __name__ == "__main__":
+    (g_aws_session, g_ec2_resource) = \
+             init('goffer-snapshotanalyzer', 'ec2')
     instances()
